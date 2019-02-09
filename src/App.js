57064982583cdf9057f17337da0af95fdd99cdec
DercilioFontes <derciliofontes@gmail.com>
2019-02-08 22:49:59 -0800

    Added styled-components and buttons.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,24 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import styled, { css } from "styled-components";
+import logo from "./logo.svg";
+import "./App.css";
+
+const Button = styled.button`
+  background: transparent;
+  border-radius: 3px;
+  border: 2px solid #8f857d;
+  color: #8f857d;
+  margin: 0 1em;
+  padding: 0.25em 1em;
+  font-size: 1.5em;
+
+  ${props =>
+    props.primary &&
+    css`
+      background: #8f857d;
+      color: white;
+    `}
+`;
 
 class App extends Component {
   render() {
@@ -10,8 +28,13 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1>Styled Components Training</h1>
         </header>
+        <br />
         <main>
-          
+          <section>
+            <h2>Button</h2>
+            <Button>Normal Button</Button>
+            <Button primary>Primary Button</Button>
+          </section>
         </main>
       </div>
     );
